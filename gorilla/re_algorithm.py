6834fa4984084740fa945be12f61500798d7dcd8
antaranin <rafal@getlevelup.com>
2018-10-05 13:16:25 +0200

    Finished

--- gorilla/re_algorithm.py
+++ gorilla/re_algorithm.py
@@ -3,7 +3,7 @@ from functools import reduce
 memorizer = {}
 
 
-def calc_val(values: dict, first_seq: str, second_seq: str) -> (int, str, str):
+def calc_vals(values: dict, first_seq: str, second_seq: str) -> (int, str, str):
     if len(first_seq) == 0 and len(second_seq) == 0:
         return 0, "", ""
 
@@ -15,7 +15,7 @@ def calc_val(values: dict, first_seq: str, second_seq: str) -> (int, str, str):
     res_vals = []
     if len(second_seq) > 0:
         second_last = last(second_seq)
-        prev_res = calc_val(values, first_seq, second_seq[:-1])
+        prev_res = calc_vals(values, first_seq, second_seq[:-1])
         first_gap_value = (
             prev_res[0] + values['*'][second_last], prev_res[1] + "-", prev_res[2] + second_last
         )
@@ -23,7 +23,7 @@ def calc_val(values: dict, first_seq: str, second_seq: str) -> (int, str, str):
 
     if len(first_seq) > 0:
         first_last = last(first_seq)
-        prev_res = calc_val(values, first_seq[:-1], second_seq)
+        prev_res = calc_vals(values, first_seq[:-1], second_seq)
         second_gap_value = (
             prev_res[0] + values[first_last]['*'], prev_res[1] + first_last, prev_res[2] + "-"
         )
@@ -32,7 +32,7 @@ def calc_val(values: dict, first_seq: str, second_seq: str) -> (int, str, str):
     if len(first_seq) > 0 and len(second_seq) > 0:
         first_last = last(first_seq)
         second_last = last(second_seq)
-        prev_res = calc_val(values, first_seq[:-1], second_seq[:-1])
+        prev_res = calc_vals(values, first_seq[:-1], second_seq[:-1])
         match_value = (prev_res[0] + values[first_last][second_last], prev_res[1] + first_last,
                        prev_res[2] + second_last)
         res_vals.append(match_value)
--- gorilla/runner.py
+++ gorilla/runner.py
@@ -1,33 +1,21 @@
-#
-# Sphinx--Snark: -8
-# KQR-------K
-# KQRIKAAKABK
-# Sphinx--Bandersnatch: 5
-# KQRK
-# K-AK
-# Snark--Bandersnatch: -18
-# KQRIKAAKABK
-# -------KA-K
-#
 import time
 from sys import stdin
 
-from re_algorithm import calc_val
 from cost_parser import parse_cost_file
 from input_parser import extract_input_data
+#iterative version
 from it_algorithm import calc_vals
+#uncomment for recursive version
+#from re_algorithm import calc_vals
 from model import SequenceRes, Sequence
 
-
-# just for testing
-# with open("data/HbB_FASTAs-out.txt") as file:
-# test_output(file, output)
 
 def process_file(input_file, values: dict):
     input_data = extract_input_data(input_file)
     results = calculate_all_with_all(input_data, values)
 
-    for res in results: print(format_output(res))
+    for res in results:
+        print(format_output(res))
 
 
 def calculate_all_with_all(inputs: list, values: dict):
@@ -47,11 +35,11 @@ def calculate_single(first_input: Sequence, second_input: Sequence, values: dict
     second_res_seq = Sequence(second_input.name, result[2])
     return SequenceRes(first_res_seq, second_res_seq, result[0])
 
+
 def format_output(output: SequenceRes) -> str:
     return f"{output.first_seq.name}--{output.second_seq.name}: {output.value}\n" \
            f"{output.first_seq.seq}\n" \
            f"{output.second_seq.seq}"
-
 
 
 in_data = stdin
@@ -60,3 +48,4 @@ vals = parse_cost_file("data/BLOSUM62.txt")
 
 process_file(in_data, vals)
 
+#If you wish to run the recursive version of the algo uncomment the import statement at the top
