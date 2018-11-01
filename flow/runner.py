65027a09b331eb6f3a7efea884fc5654595ddc2a
antaranin <rafal@getlevelup.com>
2018-11-01 20:14:49 +0100

    Final small changes.

--- flow/runner.py
+++ flow/runner.py
@@ -1,17 +1,36 @@
+from sys import stdin
+
 from algorithm import maximum_flow
-from input_parser import parse_input_to_model, parse_vertexes
+from input_parser import parse_input_to_model
 
 
-def print_result(res: list, ver: dict):
-    data = [(ver[r.origin], ver[r.destination], str(r.flow), str(r.capacity)) for r in res]
-    data.insert(0, ("From", "To", "Flow", "Capacity"))
-    col_width = max(len(word) for row in data for word in row) + 2  # padding
-    for row in data:
-        print("".join(word.ljust(col_width) for word in row))
+# def print_result(res: list, ver: dict):
+#     data = [(ver[r.origin], str(r.origin), ver[r.destination], str(r.destination), str(r.flow), str(r.capacity)) for r in res]
+#     data.insert(0, ("From", "From id", "To", "To id", "Flow", "Capacity"))
+#     col_width = max(len(word) for row in data for word in row) + 2  # padding
+#     for row in data:
+#         print("".join(word.ljust(col_width) for word in row))
+#
+# def print_full_capacities(res: list, ver: dict):
+#     data = [(ver[r.origin], str(r.origin), ver[r.destination], str(r.destination), str(r.flow), str(r.capacity)) for r in res if r.flow == r.capacity]
+#     data.insert(0, ("From", "From id", "To", "To id", "Flow", "Capacity"))
+#     col_width = max(len(word) for row in data for word in row) + 2  # padding
+#     for row in data:
+#         print("".join(word.ljust(col_width) for word in row))
 
 
-vertexes = parse_vertexes()
-edges = parse_input_to_model()
+
+# def calc_flow(res: list) -> int:
+#     return sum(r.flow for r in res if r.destination == 54)
+
+
+cmd_input = stdin
+input_res = parse_input_to_model(cmd_input)
+edges = input_res.edges
+vertexes = input_res.vertexes
 
 result = maximum_flow(edges, 0, 54)
-print_result(result, vertexes)
+
+for r in result:
+    print(r.origin, r.destination, r.flow)
+
