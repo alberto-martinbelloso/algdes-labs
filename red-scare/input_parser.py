2c0df190e958a42a8e3660de5398212a6b8eaaf5
Sabin Rimniceanu <sabin.rimniceanu@gmail.com>
2018-11-12 12:33:09 +0100

    * fixed vertex extraction method

--- red-scare/input_parser.py
+++ red-scare/input_parser.py
@@ -17,18 +17,18 @@ def parse_input(file) -> Graph:
                 first_last_vertex_split = line_split
         elif(len(line_split)==3):
             if(line_split[1]=='--'):
-                origin_v = (x for x in vertexes if x.name == line_split[0])
-                end_v = (y for y in vertexes if y.name == line_split[2])
+                origin_v = next((x for x in vertexes if x.name == line_split[0]), None)
+                end_v = next((y for y in vertexes if y.name == line_split[2]), None)
                 edges.append(Edge(origin_v, end_v))
                 edges.append(Edge(end_v, origin_v))
             elif(line_split[1]=='->'):
-                edges.append(Edge((x for x in vertexes if x.name == line_split[0]), \
-                                  (y for y in vertexes if y.name == line_split[2])))
+                edges.append(Edge(next((x for x in vertexes if x.name == line_split[0]), None), \
+                                  next((y for y in vertexes if y.name == line_split[2]), None)))
             else:
                 number_red_vertex = line.split()[2]
 
-    start_vertex = (x for x in vertexes if x.name == first_last_vertex_split[0])
-    end_vertex = (y for y in vertexes if y.name == first_last_vertex_split[1])
+    start_vertex = next((x for x in vertexes if x.name == first_last_vertex_split[0]), None)
+    end_vertex = next((y for y in vertexes if y.name == first_last_vertex_split[1]), None)
     return Graph(start_vertex, end_vertex, vertexes, edges, number_red_vertex)
 
 
