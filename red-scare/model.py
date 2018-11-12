0f74183aec0ab6463e97bf9e780042a6335501b6
Sabin Rimniceanu <sabin.rimniceanu@gmail.com>
2018-11-12 11:02:28 +0100

    * Added Graph object to model

--- red-scare/model.py
+++ red-scare/model.py
@@ -7,7 +7,7 @@ class Edge:
         self.destination = destination
 
     def __str__(self) -> str:
-        return f"(origin: {self.origin}, destination: {self.destination}, "
+        return f"(origin: {self.origin}, destination: {self.destination})"
 
     def __repr__(self) -> str:
         return self.__str__()
@@ -22,7 +22,29 @@ class Vertex:
         self.is_red = is_red
 
     def __str__(self) -> str:
-        return f"(name: {self.name}, is_red: {self.is_red}, "
+        return f"(name: {self.name}, is_red: {self.is_red})"
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+class Graph:
+    number_red_vertex: int
+    start_vertex = None
+    end_vertex = None
+    vertex_list = None
+    edge_list = None
+
+    def __init__(self, start_vertex, end_vertex, vertex_list, edge_list, number_red_vertex) -> None:
+        self.number_red_vertex = number_red_vertex
+        self.start_vertex = start_vertex
+        self.end_vertex = end_vertex
+        self.vertex_list = vertex_list
+        self.edge_list = edge_list
+
+    def __str__(self) -> str:
+        return f"(number_red_vertex: {self.number_red_vertex}, start_vertex: {self.start_vertex}, " \
+               f"end_vertex: {self.end_vertex}, vertex_list: {self.vertex_list}, edge_list: {self.edge_list})"
 
     def __repr__(self) -> str:
         return self.__str__()
