f1d927fa27da85f2ab78ec8a2c5f6e9b62edf563
antaranin <rafal@getlevelup.com>
2018-11-12 14:30:13 +0100

    Fixed the Alternate.

--- red-scare/alternate.py
+++ red-scare/alternate.py
@@ -1,20 +1,24 @@
-from model import Edge, Vertex, Graph
-from input_parser import parse_input
-from nx_parser import parse_to_nx
 import networkx as nx
-from sys import stdin
+
+from model import Graph
+from nx_parser import parse_to_nx
+
 
 def alternate(graph: Graph) -> bool:
-    for edge in graph.edge_list:
-        if (edge.origin.is_red and edge.destination.is_red) or (not edge.origin.is_red and not edge.destination.is_red):
-            graph.edge_list.remove(edge)
-    G = parse_to_nx(graph.vertex_list,graph.edge_list)
+    alternating_edges = only_alternating_edges(graph.edge_list)
+    G = parse_to_nx(graph.vertex_list, alternating_edges)
     try:
         s = nx.shortest_path(G, graph.start_vertex.name, graph.end_vertex.name)
         return True
     except nx.exception.NetworkXNoPath:
-        return False    
+        return False
 
-# the_file = stdin
+    # the_file = stdin
+
+
 # graph = parse_input(the_file)
 # print(alternate(graph))
+
+
+def only_alternating_edges(edges: list) -> list:
+    return [edge for edge in edges if edge.origin.is_red != edge.destination.is_red]
--- red-scare/runner.py
+++ red-scare/runner.py
@@ -1,11 +1,25 @@
 from sys import stdin
 
+from alternate import alternate
+from few import few
 from input_parser import parse_input
 from many import many
 from none import none
+from some import some
 
 input_data = stdin
 
 graph = parse_input(input_data)
-many_res = none(graph)
-print(many_res)
+none_res = none(graph)
+some_res = some(graph)
+few_res = few(graph)
+many_res = many(graph)
+alternate_res = alternate(graph)
+
+print(f"None => {none_res}")
+print(f"Some => {some_res}")
+print(f"Few => {few_res}")
+print(f"Many => {many_res}")
+print(f"Alternate => {alternate_res}")
+
+
